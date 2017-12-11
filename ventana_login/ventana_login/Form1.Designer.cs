bfc5ba6273166e43da50bef58411421191aa9e52
Adan Cuevas <adancuevas@outlook.com>
2017-12-10 22:50:54 -0600

    hacer commit

---
+++ ventana_login/ventana_login/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace ventana_login
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn_salir = new System.Windows.Forms.Button();
+            this.btn_iniciar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_id = new System.Windows.Forms.TextBox();
+            this.txt_contra = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_salir
+            // 
+            this.btn_salir.Location = new System.Drawing.Point(192, 216);
+            this.btn_salir.Name = "btn_salir";
+            this.btn_salir.Size = new System.Drawing.Size(75, 23);
+            this.btn_salir.TabIndex = 4;
+            this.btn_salir.Text = "Salir";
+            this.btn_salir.UseVisualStyleBackColor = true;
+            this.btn_salir.Click += new System.EventHandler(this.btn_salir_Click);
+            // 
+            // btn_iniciar
+            // 
+            this.btn_iniciar.Location = new System.Drawing.Point(24, 216);
+            this.btn_iniciar.Name = "btn_iniciar";
+            this.btn_iniciar.Size = new System.Drawing.Size(75, 23);
+            this.btn_iniciar.TabIndex = 3;
+            this.btn_iniciar.Text = "Iniciar";
+            this.btn_iniciar.UseVisualStyleBackColor = true;
+            this.btn_iniciar.Click += new System.EventHandler(this.btn_iniciar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(97, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 18);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Formulario";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(48, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "ID:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(48, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "contrasena:";
+            // 
+            // txt_id
+            // 
+            this.txt_id.BackColor = System.Drawing.SystemColors.Info;
+            this.txt_id.Location = new System.Drawing.Point(128, 88);
+            this.txt_id.MaxLength = 8;
+            this.txt_id.Name = "txt_id";
+            this.txt_id.Size = new System.Drawing.Size(100, 20);
+            this.txt_id.TabIndex = 1;
+            this.txt_id.TextChanged += new System.EventHandler(this.txt_id_TextChanged);
+            // 
+            // txt_contra
+            // 
+            this.txt_contra.BackColor = System.Drawing.SystemColors.Info;
+            this.txt_contra.Location = new System.Drawing.Point(128, 152);
+            this.txt_contra.MaxLength = 8;
+            this.txt_contra.Name = "txt_contra";
+            this.txt_contra.PasswordChar = '*';
+            this.txt_contra.Size = new System.Drawing.Size(100, 20);
+            this.txt_contra.TabIndex = 2;
+            this.txt_contra.TextChanged += new System.EventHandler(this.txt_contra_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.txt_contra);
+            this.Controls.Add(this.txt_id);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_iniciar);
+            this.Controls.Add(this.btn_salir);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Altro";
+            this.Activated += new System.EventHandler(this.Form1_Activated);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_salir;
+        private System.Windows.Forms.Button btn_iniciar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_id;
+        private System.Windows.Forms.TextBox txt_contra;
+    }
+}
+
---
+++ ventana_login/ventana_login/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ventana_login
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            txt_id.Focus();
+
+            if (File.Exists("Login.txt"))
+            {
+
+            }
+            else
+            {
+                TextWriter inicio;
+
+                inicio = new StreamWriter("Login.txt");
+                inicio.WriteLine("Usuario \t contrasena" + "\n");
+                inicio.Close();
+            }
+        }
+
+        private void btn_salir_Click(object sender, EventArgs e)
+        {
+            this.Close();   
+        }
+
+        private void btn_iniciar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txt_contra.Text) || string.IsNullOrEmpty(txt_id.Text))
+            {
+
+                MessageBox.Show("Debe completar la informacion");
+
+                btn_iniciar.Enabled = false;
+
+                return;
+
+            }
+            else
+            {
+                StreamWriter escribir = File.AppendText("Login.txt");
+
+                escribir.WriteLine(txt_id.Text + "\t\t" + txt_contra.Text);
+
+                escribir.Close();
+
+                txt_id.Clear();
+                txt_contra.Clear();
+
+                MessageBox.Show("Usuario guardado con exito en la base de datos");
+                
+            }
+            
+        }
+
+        private void txt_contra_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txt_id.Text) == false)
+            {
+                btn_iniciar.Enabled = true;
+            }
+        }
+
+        private void txt_id_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txt_contra.Text) == false)
+            {
+                btn_iniciar.Enabled = true;
+            }
+        }
+    }
+}
